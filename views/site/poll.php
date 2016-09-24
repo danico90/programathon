5ab9ea8dfc051b3d1a905a723d93d492049fe5bd
Esteban Torner Meza <etorner@konradgroup.com>
2016-09-24 17:32:01 -0400

    [poll] adding col

--- views/site/poll.php
+++ views/site/poll.php
@@ -5,7 +5,6 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-use yii\captcha\Captcha;
 
 $this->title = 'Poll';
 ?>
@@ -13,14 +12,15 @@ $this->title = 'Poll';
     <h1><?= Html::encode($this->title) ?></h1>
     <?php  
 	    foreach($model as $question){
-	    	echo '<div class="question-row">';
-	        echo '<label>'.$question->question.'</label>';
-
-	    	foreach($question->anwers as $anwers){
-	    		echo '<div class="anwers-row">';
-	    		echo '<input type="radio" name="'.$question->id.'" value="'.$anwers->value.'">'.$anwers->name;
-	    		echo '</div>';
-	    	}
+	    	echo '<div class="question-row row">';
+	        echo '<label class="col-sm-4">'.$question->question.'</label>';
+			echo '<div class="anwers-row col-sm-8">';
+		    	foreach($question->anwers as $anwers){
+		    		
+		    		echo '<input type="radio" name="'.$question->id.'" value="'.$anwers->value.'">'.$anwers->name;
+		    		echo '</br>';
+		    	}
+	    	echo '</div>';
 	    	echo '</div></br>';
 	    } 
 	    
