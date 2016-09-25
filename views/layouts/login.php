c863511504d658f364476c7c41d2880bee8dbd9e
rcanessa <rcanessa89@hotmail.com>
2016-09-25 13:04:47 -0600

    Login update

---
+++ views/layouts/login.php
@@ -0,0 +1,34 @@
+
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+   <meta charset="<?= Yii::$app->charset ?>">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <?= Html::csrfMetaTags() ?>
+   <title>La Voz del Cliente</title>
+   <?php $this->head() ?>
+</head>
+<body class="login-page">
+   <div class="wrapper">
+       <?php $this->beginBody() ?>
+           <h1 class="title login-text">La Voz del Cliente</h1>
+           <?= $content ?>
+       <?php $this->endBody() ?>
+   </div>
+</body>
+</html>
+<?php $this->endPage() ?>
--- views/site/login.php
+++ views/site/login.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /* @var $this yii\web\View */
@@ -17,55 +18,55 @@ $this->title = 'Login';
 ?>
 
 <?php if(isset($_GET['success'])) : ?>
-    <div class="alert alert-success" role="alert">Información guardada/actualizada con éxito</div>
+   <div class="alert alert-success" role="alert">Información guardada/actualizada con éxito</div>
 <?php endif; ?>
+   <?php $form = ActiveForm::begin([
+       'id' => 'login-form',
+       'options' => ['class' => 'form-horizontal site-login login', 'name' => 'login-form'],
+       'fieldConfig' => [
+           'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+           'labelOptions' => ['class' => 'col-lg-1 control-label'],
+       ],
+   ]); ?>
 
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-    
-    <p>Please fill out the following fields to login:</p>
+       <p class="title"><?= Html::encode($this->title) ?></p>
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'options' => ['class' => 'form-horizontal'],
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]); ?>
+       <?= $form->field($model, 'comercialName')->textInput() ?>
+       <i class="fa fa-comercio"></i>
 
-        <?= $form->field($model, 'comercialName')->textInput() ?>
+       <?= $form->field($model, 'country')
+       ->dropDownList(
+           ArrayHelper::map(Pais::find()->all(), 'Id', 'Nombre'),           // Flat array ('id'=>'label')
+           ['prompt'=>'']    // options
+       )
+       ?>
 
-        
+       <?= $form->field($model, 'username')->textInput() ?>
+       <i class="fa fa-user"></i>
+       <?= $form->field($model, 'password')->passwordInput() ?>
+       <i class="fa fa-key"></i>
+       <a class="pull-left" href="<?= Url::toRoute(['pyme/create']);?>">Registro</a>
+       <a class="pull-right" href="mailto:contacto@fundes.com" onclick="">Olvidé la contraseña</a>
+       <button>
+         <i class="spinner"></i>
+         <span class="state">Ingresar</span>
+       </button>
+       <div class="close" tabindex="1"><input type="button" value="X" class="close-input" onClick="this.form.reset()"/></div>
+<!--         <div class="form-group">
+           <div class="col-lg-offset-1 col-xs-4 col-lg-2">
+               <?= Html::submitButton('Cancel', ['class' => 'btn btn-primary', 'name' => 'login-button', 'onclick'=>'return false;']) ?>
+           </div>
+           <div class="col-xs-4 col-lg-2">
+               <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+           </div>
+       </div>
+       <div class="form-group">
+           <div class="col-lg-offset-1 col-xs-4 col-lg-11">
+               <a href="<?= Url::toRoute(['pyme/create']);?>">Registro</a>
+           </div>
+           <div class="col-lg-offset-1 col-sm-4 col-xs-4 col-lg-11">
+               <a href="mailto:contacto@fundes.com" onclick="">Olvidé la contraseña</a>
+           </div>
+       </div> -->
 
-        <?= $form->field($model, 'country')
-        ->dropDownList(
-            ArrayHelper::map(Pais::find()->all(), 'Id', 'Nombre'),           // Flat array ('id'=>'label')
-            ['prompt'=>'']    // options
-        )
-        ?>
-
-        <?= $form->field($model, 'username')->textInput() ?>
-
-        <?= $form->field($model, 'password')->passwordInput() ?>
-
-        
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-xs-4 col-lg-2">
-                <?= Html::submitButton('Cancel', ['class' => 'btn btn-primary', 'name' => 'login-button', 'onclick'=>'return false;']) ?>
-            </div>
-            <div class="col-xs-4 col-lg-2">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-xs-4 col-lg-11">
-                <a href="<?= Url::toRoute(['pyme/create']);?>">Registro</a>
-            </div>
-            <div class="col-lg-offset-1 col-sm-4 col-xs-4 col-lg-11">
-                <a href="mailto:contacto@fundes.com" onclick="">Olvidé la contraseña</a>
-            </div>
-        </div>
-
-    <?php ActiveForm::end(); ?>
-</div>
+   <?php ActiveForm::end(); ?>
